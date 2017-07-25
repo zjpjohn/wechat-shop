a4f3ab3e001066974d4f156979d4f4fdfec1a89d
weixiao836 <wx_836@163.com>
2017-07-25 22:55:18 +0800

    添加首页

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
-    <shop-header></shop-header>
     <shop-nav></shop-nav>
   </div>
 </template>
 
 <script>
 import nav from './components/nav/nav'
-import header from './components/header/header'
-
 export default {
   name: 'app',
   components:{
      "shop-nav":nav,
-     "shop-header":header
   }
 }
 </script>
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <shop-header></shop-header>
+</template>
+
+<script>
+  import ShopHeader from "../components/header/header";
+
+  export default {
+      name:"index",
+      components:{
+        ShopHeader
+      }
+  }
+</script>
+
+<style>
+</style>
