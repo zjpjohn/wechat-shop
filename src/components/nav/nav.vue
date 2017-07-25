81b75ac66417da6e3d485c16c3b5eec73bfd803f
weixiao836 <wx_836@163.com>
2017-07-25 21:47:56 +0800

    nav组件样式修改

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -1,33 +1,33 @@
 <template>
   <nav class="nav">
-    <div class="nav-item icon" key="1">
+    <div class="nav-item" key="1">
         <router-link to="/index">
-            <span class="home"></span>
+            <span class="icon home"></span>
           <p>首页</p>
 
         </router-link>
     </div>
-    <div class="nav-item icon" key="2">
+    <div class="nav-item" key="2">
       <router-link to="/all">
-        <span class="home"></span>
+        <span class="icon select"></span>
         <p>全部精选</p>
     </router-link>
     </div>
-    <div class="nav-item icon" key="3">
+    <div class="nav-item" key="3">
       <router-link to="/free">
-        <span class="home"></span>
+        <span class="icon free"></span>
        <p> 使用活动</p>
     </router-link>
     </div>
-    <div class="nav-item icon" key="4">
+    <div class="nav-item" key="4">
       <router-link to="/cart">
-        <span class="home"></span>
+        <span class="icon cart"></span>
         <p>购物车</p>
     </router-link>
     </div>
-    <div class="nav-item icon" key="5">
+    <div class="nav-item" key="5">
       <router-link to="/user">
-        <span class="home"></span>
+        <span class="icon user"></span>
         <p>个人中心</p>
     </router-link>
     </div>
@@ -59,24 +59,66 @@
       font-size: 0.2rem;
       justify-content: center;
     }
-    .icon {
-      background-size: 0.36rem 0.36rem;
-      //background-image: url(../../assets/img/nav/select.png);
-      .home{
-        background: url(../../assets/img/nav/home.png) no-repeat center;
+      .icon{
         display: inline-block;
         width:0.36rem;
         height:0.36rem;
+        margin:0 auto 0.05rem;
+      }
+      //background-image: url(../../assets/img/nav/select.png);
+      .home{
+        background: url(../../assets/img/nav/home.png) no-repeat center;
+        background-size: cover;
+      }
+      .select{
+        background: url(../../assets/img/nav/select.png) no-repeat center;
+        background-size: cover;
+      }
+      .free{
+        background: url(../../assets/img/nav/free.png) no-repeat center;
+        background-size: cover;
+      }
+      .user{
+        background: url(../../assets/img/nav/user.png) no-repeat center;
+        background-size: cover;
+      }
+      .cart{
+        background: url(../../assets/img/nav/cart.png) no-repeat center;
+        background-size: cover;
       }
       .router-link-active{
+        color: #b6e428;
+        .icon{
+          display: inline-block;
+          width:0.36rem;
+          height:0.36rem;
+          margin:0 auto 0.05rem;
+        }
         .home{
           background: url(../../assets/img/nav/home_active.png) no-repeat center;
-          display: inline-block;
-          width:100%;
-          height:0.36rem;
+          background-size: cover;
+        }
+        .select{
+          background: url(../../assets/img/nav/select_active.png) no-repeat center;
+          background-size: cover;
+        }
+        .free{
+          background: url(../../assets/img/nav/free_active.png) no-repeat center;
+          background-size: cover;
+        }
+        .user{
+          background: url(../../assets/img/nav/user_active.png) no-repeat center;
+          background-size: cover;
+        }
+        .cart{
+          background: url(../../assets/img/nav/cart_active.png) no-repeat center;
+          background-size: cover;
         }
       }
-    }
+      .select{
+
+      }
+
     a{
       display: flex;
       height: 0.8rem;
